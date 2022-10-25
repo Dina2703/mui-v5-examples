18e4e89941c97c4bf4d5d3093e8bfd82168a0a1f
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-10-25 19:01:27 -0400

    Theme & custome a theme, typography, color

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ function App() {
   return (
     <div className="App">
       <h2>Hello</h2>
-      <Button variant="contained">Click Me</Button>
+      <Button variant="contained" color="info" sx={{ p: 2 }}>
+        Click Me
+      </Button>
+      <Button variant="contained" color="primary">
+        Custom theme color
+      </Button>
+      <Button variant="contained" color="secondary">
+        Custom theme color 2
+      </Button>
       <Typography variant="subtitle2" color="initial">
         Typography
+      </Typography>
+      <Typography variant="myVariant">Custom theme Typography</Typography>
+      <Typography sx={{ color: "myCustomColor.superDark" }}>
+        Custom theme Typography via SX
       </Typography>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { orange, red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#CCC",
+    },
+    secondary: {
+      main: orange[500],
+    },
+    myCustomColor: {
+      main: red[400],
+      superDark: red[800],
+      superLight: red[100],
+    },
+  },
+  typography: {
+    myVariant: {
+      fontSize: "6rem",
+      color: orange[800],
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
